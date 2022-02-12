1ccd0d6360977cf5c972b89cb6bc381825d2d07f
pedrodahmer <pedrohd1213@gmail.com>
2022-02-12 19:36:27 -0300

    update readme

--- src/utils/helpers/query-post-by-uid.ts
+++ src/utils/helpers/query-post-by-uid.ts
@@ -3,7 +3,7 @@ import { RichText } from 'prismic-reactjs'
 
 export const queryPostByUID = async (client: Client, slug: string) => {
   const response = await client.getByUID('post', slug)
-
+  
   const post = {
     slug: response.uid,
     title: RichText.asText(response.data.title),
